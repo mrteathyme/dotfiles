f262e227dbcc226aae83bac70223995e28c241d1
mrteathyme <mrteathyme@gmail.com>
2023-08-25 04:22:45 +1000

    added crates.nvim

---
+++ nvim/lua/config/crates.lua
@@ -0,0 +1,16 @@
+local function init()
+require('crates').setup {
+    ...
+    src = {
+        ...
+        coq = {
+            enabled = true,
+            name = "crates.nvim",
+        },
+    },
+}
+end
+
+return {
+	init = init 
+}
