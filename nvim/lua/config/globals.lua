acadaa6fa975f0c52c9126f4ca1131486ef8b840
mrteathyme <mrteathyme@gmail.com>
2023-08-27 16:44:03 +1000

    Fixed line number color

--- nvim/lua/config/globals.lua
+++ nvim/lua/config/globals.lua
@@ -1,7 +1,7 @@
 local function init()
 	vim.wo.relativenumber = true
 	vim.wo.number = true
-	vim.api.nvim_set_hl(0, 'LineNr', { fg = yourcolor} )
+	vim.api.nvim_set_hl(0, 'LineNr', { fg = "white"} )
 	vim.wo.colorcolumn = "80,120"
 end                                                                           
 
